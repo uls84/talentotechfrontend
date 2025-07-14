6832503094c76df4877ded0ee1d6ca177b4136c0
uls84 <raul.eme84@gmail.com>
2025-07-14 15:43:44 -0300

    modificando carrito nuevo

--- JS/carrito3.js
+++ JS/carrito3.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    cargarCarrito();
+});
+
 const btnCart = document.querySelector('.container-cart-icon');
 const containerCartProducts = document.querySelector(
 	'.container-cart-products'
@@ -6,6 +10,59 @@ const containerCartProducts = document.querySelector(
 btnCart.addEventListener('click', () => {
 	containerCartProducts.classList.toggle('hidden-cart');
 });
+
+// Vaciar carrito
+document.getElementById('vaciar-carrito').addEventListener('click',
+    function () {
+        localStorage.removeItem('carrito');
+        cargarCarrito();
+    });
+
+
+function agregarProducto(event) {
+    let producto = {
+        id: event.target.getAttribute('data-id'),
+        nombre: event.target.getAttribute('data-nombre'),
+        precio: event.target.getAttribute('data-precio')
+    };
+
+
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    carrito.push(producto);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    cargarCarrito();
+}
+
+function cargarCarrito() {
+    let listaCarrito = document.getElementById('lista-carrito');
+    listaCarrito.innerHTML = '';
+
+    let totalCompra = document.getElementById('totalCompra');
+    totalCompra.innerHTML = '';
+
+    let total = 0;
+
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+
+    for (let i = 0; i < carrito.length; i++) {
+        let producto = carrito[i];
+
+
+        let li = document.createElement('li');
+        li.textContent = producto.nombre + ' - $' + producto.precio;
+        listaCarrito.appendChild(li);
+
+        total +=parseInt(producto.precio);
+
+    }
+
+    if (carrito != 0) {
+        let h2 = document.createElement('h2');
+        h2.textContent = "$" + total.toLocaleString();
+        totalCompra.appendChild(h2);
+    }
+}
 
 /* ========================= */
 const cartInfo = document.querySelector('.cart-product');
